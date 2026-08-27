17ecc39f3b796231dc784cbf211df9e58c369ffd
jlzdev <163107032+jlzdev@users.noreply.github.com>
2026-08-27 17:02:25 +0200

    new icons

--- vite.config.ts
+++ vite.config.ts
@@ -20,8 +20,10 @@ export default defineConfig({
         background_color: '#0e1420',
         theme_color: '#0e1420',
         icons: [
-          { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
+          { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
+          { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
+          { src: 'icons/icon-maskable-192.png', sizes: '192x192', type: 'image/png', purpose: 'maskable' },
+          { src: 'icons/icon-maskable-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
         ],
       },
     }),
